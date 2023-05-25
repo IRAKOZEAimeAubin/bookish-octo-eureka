ecbdf54770a988de823bae58e5784ab8850195db
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-05-25 18:32:09 +0200

    feat: commit #2

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,29 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="flex flex-col w-[100%] items-center mt-4 gap-3">
+    <h1 class="text-center bg-amber-200 text-slate-950 w-28 p-2 rounded-md font-extrabold">{{ title }}</h1>
+    <HelloWorld />
+    <input type="text" ref="name" class="border-slate-400 border-[1px] rounded-sm outline-none py-1 px-2 placeholder:italic" placeholder="John Doe...">
+    <button class="bg-sky-800 text-slate-200 px-3 py-2 rounded-md hover:text-slate-50 hover:bg-sky-700" @click="handleClick">Click Me</button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+        title:"Brainy says: "
+      }
+  },
+  methods: {
+    handleClick () {
+      console.log( this.$refs.name.value )
+      }
+    }
+  }
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1 class="text-center text-lg font-serif font-bold">Hello Vue</h1>
+  <h2 class="text-center text-lg font-serif font-bold text-slate-950">Hello Vue</h2>
 </template>
+
+<script>
+export default {
+  name:"HelloWorld"
+}
+</script>
